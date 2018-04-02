e1cb40943659425b3060a4146a91737bff29cced
ToshikiImagawa <shibainusamulai@gmail.com>
2018-04-03 02:40:12 +0900

    [add] add Blend Animation

---
+++ Assets/TextEffect/TextBlendPosition.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace TextEffect
+{
+    public class TextBlendPosition : TextEffectBase
+    {
+        [SerializeField, Range(0f, 1f)] private float _blend;
+        [SerializeField] private float _interval;
+        [SerializeField] private Vector2 _fromDistance = Vector2.zero;
+        [SerializeField] private Vector2 _toDistance = Vector2.zero;
+        [SerializeField] private AlignmentType _alignment = AlignmentType.MiddleCenter;
+        private Vector2 _cacheDistance;
+
+        /// <summary>
+        /// ブレンド割合
+        /// </summary>
+        public float Blend
+        {
+            get { return _blend; }
+            set
+            {
+                _blend = value;
+                SetVerticesDirty();
+            }
+        }
+
+        /// <summary>
+        /// インターバル
+        /// </summary>
+        public float Interval
+        {
+            get { return _interval; }
+            set
+            {
+                _interval = value;
+                SetVerticesDirty();
+            }
+        }
+
+        /// <summary>
+        /// 開始
+        /// </summary>
+        public Vector2 FromDistance
+        {
+            get { return _fromDistance; }
+            set
+            {
+                _fromDistance = value;
+                SetVerticesDirty();
+            }
+        }
+
+        /// <summary>
+        /// 終了
+        /// </summary>
+        public Vector2 ToDistance
+        {
+            get { return _toDistance; }
+            set
+            {
+                _toDistance = value;
+                SetVerticesDirty();
+            }
+        }
+
+#if UNITY_EDITOR
+        protected override void OnValidate()
+        {
+            base.OnValidate();
+            FromDistance = _fromDistance;
+            ToDistance = _toDistance;
+            Blend = _blend;
+            Interval = _interval;
+        }
+#endif
+
+        protected override void Modify(ref List<UIVertex> stream)
+        {
+            var width = RectTransform.rect.width;
+            var height = RectTransform.rect.height;
+            var pivot = RectTransform.pivot;
+
+            var count = 0;
+            var streamCount = stream.Count;
+            var startPoint = (streamCount / 6 + Interval * 2) * Blend - Interval;
+
+            for (int i = 0; i < streamCount; i += 6)
+            {
+                var distance = count < startPoint ? ToDistance :
+                    count > startPoint + Interval ? FromDistance :
+                    FromDistance + (ToDistance - FromDistance) * (startPoint + Interval - count) / Interval;
+
+                var anchor = GetAnchor(stream.ToArray(), i, _alignment);
+                var newCenter = anchor + distance;
+                var distanceCenter = new Vector2(
+                    newCenter.x > 0
+                        ? (newCenter.x + width * pivot.x) % width - width * pivot.x
+                        : (newCenter.x - width + width * pivot.x) % width + width - width * pivot.x,
+                    newCenter.y > 0
+                        ? (newCenter.y + height * pivot.y) % height - height * pivot.y
+                        : (newCenter.y - height + height * pivot.y) % height + height - height * pivot.y
+                );
+
+                for (var r = 0; r < 6; r++)
+                {
+                    var element = stream[i + r];
+                    var pos = element.position - (Vector3)anchor;
+                    var newPosition = (Vector2)pos + distanceCenter;
+                    element.position = newPosition;
+
+                    stream[i + r] = element;
+                }
+                count ++;
+            }
+        }
+    }
+}
---
+++ Assets/TextEffect/TextBlendRotation.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TextEffect
+{
+    public class TextBlendRotation : TextEffectBase
+    {
+        [SerializeField, Range(0f, 1f)] private float _blend;
+        [SerializeField] private float _interval;
+        [SerializeField] private float _fromAngle;
+        [SerializeField] private float _toAngle;
+        [SerializeField] private AlignmentType _alignment = AlignmentType.MiddleCenter;
+
+        /// <summary>
+        /// ブレンド割合
+        /// </summary>
+        public float Blend
+        {
+            get { return _blend; }
+            set
+            {
+                _blend = value;
+                SetVerticesDirty();
+            }
+        }
+
+        /// <summary>
+        /// インターバル
+        /// </summary>
+        public float Interval
+        {
+            get { return _interval; }
+            set
+            {
+                _interval = value;
+                SetVerticesDirty();
+            }
+        }
+
+        /// <summary>
+        /// 開始
+        /// </summary>
+        public float FromAngle
+        {
+            get { return _fromAngle; }
+            set
+            {
+                _fromAngle = value;
+                SetVerticesDirty();
+            }
+        }
+
+        /// <summary>
+        /// 終了
+        /// </summary>
+        public float ToAngle
+        {
+            get { return _toAngle; }
+            set
+            {
+                _toAngle = value;
+                SetVerticesDirty();
+            }
+        }
+#if UNITY_EDITOR
+        protected override void OnValidate()
+        {
+            base.OnValidate();
+            ToAngle = _toAngle;
+            FromAngle = _fromAngle;
+            Blend = _blend;
+            Interval = _interval;
+        }
+#endif
+
+        protected override void Modify(ref List<UIVertex> stream)
+        {
+            var count = 0;
+            var streamCount = stream.Count;
+            var startPoint = (streamCount / 6 + Interval * 2) * Blend - Interval;
+
+            for (int i = 0; i < streamCount; i += 6)
+            {
+                var angle = count < startPoint ? ToAngle :
+                    count > startPoint + Interval ? FromAngle :
+                    FromAngle + (ToAngle - FromAngle) * (startPoint + Interval - count) / Interval;
+
+                var anchor = GetAnchor(stream.ToArray(), i, _alignment);
+
+                for (var r = 0; r < 6; r++)
+                {
+                    var element = stream[i + r];
+
+                    var pos = element.position - (Vector3)anchor;
+                    var newPos = new Vector2(
+                        pos.x * Mathf.Cos(angle * Mathf.Deg2Rad) - pos.y * Mathf.Sin(angle * Mathf.Deg2Rad),
+                        pos.x * Mathf.Sin(angle * Mathf.Deg2Rad) + pos.y * Mathf.Cos(angle * Mathf.Deg2Rad));
+                    element.position = newPos + anchor;
+
+                    stream[i + r] = element;
+                }
+                count++;
+            }
+        }
+    }
+}
